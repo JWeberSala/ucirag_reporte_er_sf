46e46192a7d0f63596449b0d4ed1ffe068eeb1fd
Jorge Weber <jorgerweber@gmail.com>
2025-09-15 09:16:34 -0300

    Determinaciones según poblacion

--- scripts/determinaciones.R
+++ scripts/determinaciones.R
@@ -7,7 +7,7 @@ library(lubridate)
 library(plotly)
 
 
-determinacion  <- base %>% select(IDEVENTOCASO, SEPI_FECHA_INTER,
+determinacion  <- base %>% select(IDEVENTOCASO,poblacion, SEPI_FECHA_INTER,
                                   COVID_19_FINAL, VSR_FINAL, INFLUENZA_FINAL)
 
 
@@ -15,7 +15,7 @@ negativos <- determinacion %>%
   filter(COVID_19_FINAL == 'Negativo',
          VSR_FINAL == 'Negativo',
          INFLUENZA_FINAL == 'Negativo') %>% 
-  select(IDEVENTOCASO, SEPI_FECHA_INTER, COVID_19_FINAL) %>% 
+  select(IDEVENTOCASO, poblacion, SEPI_FECHA_INTER, COVID_19_FINAL) %>% 
   rename(Determinacion = COVID_19_FINAL)
 
 
@@ -24,23 +24,23 @@ negativos <- determinacion %>%
 
 
 vsr <- determinacion %>% filter(VSR_FINAL != 'Negativo') %>% 
-       select(IDEVENTOCASO, SEPI_FECHA_INTER, VSR_FINAL) %>% 
+       select(IDEVENTOCASO,poblacion, SEPI_FECHA_INTER, VSR_FINAL) %>% 
        rename(Determinacion = VSR_FINAL)
 
 
 influenza <- determinacion %>% filter(INFLUENZA_FINAL != 'Negativo') %>% 
-             select(IDEVENTOCASO, SEPI_FECHA_INTER, INFLUENZA_FINAL) %>% 
+             select(IDEVENTOCASO,poblacion, SEPI_FECHA_INTER, INFLUENZA_FINAL) %>% 
               rename(Determinacion = INFLUENZA_FINAL)
 
 
 sar_cov <- determinacion %>% filter(COVID_19_FINAL != 'Negativo') %>% 
-           select(IDEVENTOCASO, SEPI_FECHA_INTER, COVID_19_FINAL) %>% 
+           select(IDEVENTOCASO, poblacion, SEPI_FECHA_INTER, COVID_19_FINAL) %>% 
            rename(Determinacion = COVID_19_FINAL)
 
 
 
 deteterm_resumen <- rbind(negativos, vsr, influenza, sar_cov) %>% 
-                    count(SEPI_FECHA_INTER, Determinacion) %>% 
+                    count(SEPI_FECHA_INTER,poblacion, Determinacion) %>% 
                     mutate(Determinacion = case_when(Determinacion == 'Sin resultado'~ 'En estudio',
                                                      Determinacion == 'Positivo' ~ 'Sars-Cov2',
                                                      T ~ Determinacion) )
@@ -61,6 +61,7 @@ g <- ggplot(deteterm_resumen) +
            stat = 'identity', width = 1) +
   theme_classic() +
   scale_x_continuous(breaks = seq(1, 52,1)) +
+  scale_y_continuous(breaks = seq(0,100,2)) +
   scale_fill_manual(values = c('#bdbdbd', '#969696', '#74a9cf', 
                                '#c2e699', '#78c679', '#238443',
                                '#fecc5c', '#fd8d3c')) +
@@ -68,7 +69,8 @@ g <- ggplot(deteterm_resumen) +
         fill = '',
         x = 'Semana de Internación', y = 'Eventos',) +
   theme(plot.title = element_text(hjust = 0.5, size = 13), legend.position = 'none')+
-  theme(legend.position = 'none')
+  theme(legend.position = 'none') +
+  facet_wrap(~ poblacion, ncol = 1)
 
 grafico_virus <- ggplotly(g, tooltip = c("text"))
 
